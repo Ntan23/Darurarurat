d2a2786e24ce3dc3984460ba05838773d5ff6ef2
Ntan23 <ntan231002@gmail.com>
2023-07-01 09:45:25 +0700

    Update (Add Settings)

---
+++ Assets/Script/UI/SettingsUI.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+public class SettingsUI : MonoBehaviour
+{
+    public AudioMixer MainMixer;
+    [SerializeField] TMP_Dropdown QualityDropdown;
+    [SerializeField] Slider BGMSlider;
+    [SerializeField] Slider SFXSlider;
+    [SerializeField] Toggle fullscreenToogle;
+    private float bgmVolume;
+    private float sfxVolume;
+    private int isFullscreen;
+    private int qualityLevel;
+
+    void Awake()
+    {
+        bgmVolume = PlayerPrefs.GetFloat("BGMVolume", 0);
+        sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0);
+        isFullscreen = PlayerPrefs.GetInt("IsFullscreen", 1);
+        qualityLevel = PlayerPrefs.GetInt("QualityLevel", 2);
+    }
+
+    private void Start()
+    {
+        BGMSlider.value = bgmVolume;
+        SFXSlider.value = sfxVolume;
+        MainMixer.SetFloat("BGM_Volume", bgmVolume);
+        MainMixer.SetFloat("SFX_Volume", sfxVolume);
+
+        if(isFullscreen == 1) 
+        {
+            fullscreenToogle.isOn = true;
+            Screen.fullScreen = true;
+        }
+        else if(isFullscreen == 0) 
+        {
+            fullscreenToogle.isOn = false;
+            Screen.fullScreen = false;
+        }
+        
+        QualityDropdown.value = qualityLevel;
+        QualitySettings.SetQualityLevel(qualityLevel);
+    }
+
+    public void UpdateBGMSound(float value)
+    {
+        MainMixer.SetFloat("BGM_Volume", value);
+        PlayerPrefs.SetFloat("BGMVolume", value);
+    }
+    public void UpdateSFXSound(float value)
+    {
+        MainMixer.SetFloat("SFX_Volume", value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    public void UpdateQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("QualityLevel", qualityIndex);
+    }
+
+    public void UpdateFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+
+        if (isFullscreen) PlayerPrefs.SetInt("IsFullscreen", 1);
+        else if (!isFullscreen) PlayerPrefs.SetInt("IsFullscreen", 0);
+    }
+
+    public void OpenSettings() => LeanTween.moveLocalY(gameObject, 0.0f, 0.8f).setEaseSpring();
+
+    public void CloseSettings() => LeanTween.moveLocalY(gameObject, -1092.0f, 0.8f).setEaseSpring();
+}
