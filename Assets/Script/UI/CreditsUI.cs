5065c1c2f8f3e3452dfdc62ea011a4e421f277a7
Ntan23 <ntan231002@gmail.com>
2023-07-06 10:47:19 +0700

    Update

--- Assets/Script/UI/CreditsUI.cs
+++ Assets/Script/UI/CreditsUI.cs
@@ -6,35 +6,21 @@ public class CreditsUI : MonoBehaviour
 {
     [SerializeField] private GameObject creditText;
     [SerializeField] private float textTimeToReachTheEnd;
-    private bool canScroll;
 
-    void Update()
-    {
-        if(canScroll) StartCoroutine(ScrollAnimation());
-    }
-
-    public void OpenCredits()
-    {
-        LeanTween.moveLocalX(gameObject, 0.0f, 0.8f).setEaseSpring();
-        canScroll = true;
-    }
+    public void OpenCredits() => StartCoroutine(OpenAnimation());
 
     public void CloseCredits() 
     {
         LeanTween.cancel(creditText); 
         LeanTween.moveLocalX(gameObject, 2075.0f, 0.8f).setEaseSpring();
         creditText.transform.localPosition = new Vector3(0.0f, -1166.0f, 0.0f);
-        canScroll = false;
     }
 
-    IEnumerator ScrollAnimation()
+    IEnumerator OpenAnimation()
     {
-        canScroll = false;
-        LeanTween.moveLocalY(creditText, 1166.0f, textTimeToReachTheEnd);
-        yield return new WaitForSeconds(textTimeToReachTheEnd + 0.1f);
-        creditText.transform.localPosition = new Vector3(0.0f, -1166.0f, 0.0f);
-        yield return new WaitForSeconds(0.1f);
-        canScroll = true;
+        LeanTween.moveLocalX(gameObject, 0.0f, 0.8f).setEaseSpring();
+        yield return new WaitForSeconds(0.8f);
+        LeanTween.moveLocalY(creditText, 1166.0f, textTimeToReachTheEnd).setLoopClamp();
     }
 }
 
