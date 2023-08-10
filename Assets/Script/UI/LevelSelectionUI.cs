818567dd2722fefe1563b8eed2bf4df681565508
Ntan23 <ntan231002@gmail.com>
2023-08-10 16:56:25 +0700

    Update

--- Assets/Script/UI/LevelSelectionUI.cs
+++ Assets/Script/UI/LevelSelectionUI.cs
@@ -13,6 +13,16 @@ public class LevelSelectionUI : MonoBehaviour
     [SerializeField] private GameObject[] levels;
     [SerializeField] private GameObject levelIndicator;
     [SerializeField] private Button previousButton;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button playButton;
+    private ScenesManager scenesManager;
+
+    void Start() 
+    {
+        scenesManager = ScenesManager.instance;
+
+        levelUnlocked = PlayerPrefs.GetInt("LevelUnlocked", 1);
+    }
 
     // void Start() 
     // {
@@ -34,30 +44,42 @@ public class LevelSelectionUI : MonoBehaviour
     //     LeanTween.moveLocalY(clipboard, 0.0f, 0.8f).setEaseSpring(); 
     // }
 
-    private void CheckForlevel()
-    {
-        levelUnlocked = PlayerPrefs.GetInt("LevelUnlocked", 1);
+    // private void CheckForlevel()
+    // {
+    //     levelUnlocked = PlayerPrefs.GetInt("LevelUnlocked", 1);
 
-        for(int i = 1; i <= levelButton.Length; i++)
-        {
-            if(i > levelUnlocked)
-            {
-                levelButton[i - 1].GetComponent<CanvasGroup>().interactable = false;
-                levelButton[i - 1].GetComponent<CanvasGroup>().alpha = 0.5f;
-            }
-        }
-    }
+    //     for(int i = 1; i <= levelButton.Length; i++)
+    //     {
+    //         if(i > levelUnlocked)
+    //         {
+    //             levelButton[i - 1].GetComponent<CanvasGroup>().interactable = false;
+    //             levelButton[i - 1].GetComponent<CanvasGroup>().alpha = 0.5f;
+    //         }
+    //     }
+    // }
 
     public void MoveNext()
     {
-        if(levelIndex < 3)
-        {
-            LeanTween.moveLocalX(levels[levelIndex], -1300.0f, 0.8f).setEaseOutExpo();
-            LeanTween.moveLocalX(levels[levelIndex + 1], 0.0f, 0.8f).setEaseOutExpo();
-            LeanTween.moveLocalX(levelIndicator.gameObject, levelIndicator.transform.localPosition.x - 191.0f, 0.8f).setEaseOutExpo();
+        if(levelIndex < 4)
+        {   
+            previousButton.interactable = false;
+            nextButton.interactable = false;
+            playButton.interactable = false;
 
-            levelIndex++;
-            if(levelIndex > 0) previousButton.interactable = true;
+            if(levelIndex == 0) previousButton.gameObject.SetActive(true);
+            if(levelIndex == 2) nextButton.gameObject.SetActive(false);
+
+            LeanTween.moveLocalX(levels[levelIndex], -1300.0f, 0.5f).setEaseOutExpo();
+            LeanTween.moveLocalX(levels[levelIndex + 1], 0.0f, 0.5f).setEaseOutExpo();
+            LeanTween.moveLocalX(levelIndicator.gameObject, levelIndicator.transform.localPosition.x - 191.0f, 0.5f).setEaseOutExpo().setOnComplete(() => 
+            {
+                levelIndex++;
+                previousButton.interactable = true;
+                nextButton.interactable = true;
+
+                if(levelIndex + 1 > levelUnlocked) playButton.interactable = false;
+                else playButton.interactable = true;
+            });
         }
     }
 
@@ -65,12 +87,26 @@ public class LevelSelectionUI : MonoBehaviour
     {
         if(levelIndex > 0)
         {
-            LeanTween.moveLocalX(levels[levelIndex], 1300.0f, 0.8f).setEaseOutExpo();
-            LeanTween.moveLocalX(levels[levelIndex - 1], 0.0f, 0.8f).setEaseOutExpo();
-            LeanTween.moveLocalX(levelIndicator.gameObject, levelIndicator.transform.localPosition.x + 191.0f, 0.8f).setEaseOutExpo();
-    
-            levelIndex--;
-            if(levelIndex == 0) previousButton.interactable = false;
+            previousButton.interactable = false;
+            nextButton.interactable = false;
+            playButton.interactable = false;
+
+            if(levelIndex == 1) previousButton.gameObject.SetActive(false);
+            if(levelIndex == 3) nextButton.gameObject.SetActive(true);
+
+            LeanTween.moveLocalX(levels[levelIndex], 1300.0f, 0.5f).setEaseOutExpo();
+            LeanTween.moveLocalX(levels[levelIndex - 1], 0.0f, 0.5f).setEaseOutExpo();
+            LeanTween.moveLocalX(levelIndicator.gameObject, levelIndicator.transform.localPosition.x + 191.0f, 0.5f).setEaseOutExpo().setOnComplete(() =>
+            {
+                levelIndex--;
+                previousButton.interactable = true;
+                nextButton.interactable = true;
+
+                if(levelIndex + 1 > levelUnlocked) playButton.interactable = false;
+                else playButton.interactable = true;
+            });
         }
     }
+
+    public void Play() => scenesManager.GoToTargetScene("Level " + (levelIndex + 1).ToString());
 }
