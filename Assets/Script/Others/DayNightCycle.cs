9b0641e550c1348cf96536dba6a3e0d7e98dd497
Ntan23 <ntan231002@gmail.com>
2024-11-04 10:06:41 +0700

    Update (Directional light (treat scene 3d) color cahnge overtime like the 2d reception scene)

--- Assets/Script/Others/DayNightCycle.cs
+++ Assets/Script/Others/DayNightCycle.cs
@@ -1,28 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-[RequireComponent(typeof(Light2D))]
 public class DayNightCycle : MonoBehaviour
 {
     private Light2D _light;
+    private Light light3D;
     [SerializeField] private GameObject roomLight;
     private TimeManager tm;
     [SerializeField] private Gradient dayNightLightColorGradient;
     private const int hoursInDay = 24;
+    [SerializeField] private bool is2D;
 
-    void Awake() => _light = GetComponent<Light2D>();
-
+    void Awake() 
+    {
+        if(is2D) _light = GetComponent<Light2D>();
+        if(!is2D) light3D = GetComponent<Light>();
+    } 
+    
     void Start() => tm = TimeManager.instance;
 
     void Update() 
     {
         ChangeLightColor();
 
-        if(tm.GetHour() >= 17) roomLight.SetActive(true);
-        if(tm.GetHour() >= 7 && tm.GetHour() <= 17) roomLight.SetActive(false);
+        if(tm.GetHour() >= 17 && is2D) roomLight.SetActive(true);
+        if(tm.GetHour() >= 7 && tm.GetHour() <= 17 && is2D) roomLight.SetActive(false);
     }
 
-    private void ChangeLightColor() => _light.color = dayNightLightColorGradient.Evaluate(PercentageOfDay());
+    private void ChangeLightColor() 
+    {
+        if(is2D) _light.color = dayNightLightColorGradient.Evaluate(PercentageOfDay());
+        if(!is2D) light3D.color = dayNightLightColorGradient.Evaluate(PercentageOfDay());
+    }
     
     private float PercentageOfDay()
     {
